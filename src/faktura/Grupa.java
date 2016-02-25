cdcc046ede7efaf0ad8928a8d1caa9c6783c806e
Reniferek <Reniferek@Reniferek-PC>
2016-02-25 20:37:06 +0100

    Start

---
+++ src/faktura/Grupa.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package faktura;
+
+import java.util.List;
+
+/**
+ *
+ * @author Reniferek
+ */
+public class Grupa {
+    private List<Student> members;
+    private String name;
+    
+}
